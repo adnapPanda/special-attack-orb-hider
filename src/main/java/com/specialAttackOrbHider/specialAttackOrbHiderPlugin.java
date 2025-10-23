c0a1ba300ddf65a8a4e598c40e39502ff17abc79
adnapPanda <robin_herlan@yahoo.com>
2025-10-23 16:38:08 +0200

    Use InterfaceID instead of static int

--- src/main/java/com/specialAttackOrbHider/specialAttackOrbHiderPlugin.java
+++ src/main/java/com/specialAttackOrbHider/specialAttackOrbHiderPlugin.java
@@ -5,6 +5,7 @@ import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.*;
 import net.runelite.api.events.ItemContainerChanged;
+import net.runelite.api.gameval.InterfaceID;
 import net.runelite.api.gameval.InventoryID;
 import net.runelite.api.widgets.Widget;
 import net.runelite.client.config.ConfigManager;
@@ -30,8 +31,6 @@ public class specialAttackOrbHiderPlugin extends Plugin
 	@Inject
 	private specialAttackOrbHiderConfig config;
 
-	private static final int SPEC_ORB_ID = 10485794;
-
 	@Override
 	protected void startUp() throws Exception
 	{
@@ -48,7 +47,7 @@ public class specialAttackOrbHiderPlugin extends Plugin
 	public void onItemContainerChanged(ItemContainerChanged itemContainerChanged)
 	{
 		// Hides the spec orb for all weapons if option is set
-		Widget SpecOrb = client.getWidget(SPEC_ORB_ID);
+		Widget SpecOrb = client.getWidget(InterfaceID.Orbs.ORB_SPECENERGY);
 		if (config.hideSpecOrbAll() && SpecOrb != null) SpecOrb.setHidden(true);
 		// Checks whether equipped weapon is specified in the list provided by player and disables spec orb if so
 		else {
